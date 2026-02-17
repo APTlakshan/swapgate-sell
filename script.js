8d205b068fe6c02b98adc96e8148b5dd8148f6be
SwapGate-store <lakshantharindu891@gmail.com>
2026-02-17 21:37:36 +0530

    done

--- script.js
+++ script.js
@@ -992,7 +992,7 @@ async function submitOrder() {
                 const caption = `Receipt for ${currentTransaction.amount} USDT - ${currentTransaction.bankDetails.accountName}`;
                 await sendFileToTelegram(e.target.result, file.name, caption);
                 
-                const finishMessage = `========= order finished ======`;
+                const finishMessage = `================= order finished ==============`;
                 await sendToTelegram(finishMessage);
                 
                 // Auto-download invoice immediately
